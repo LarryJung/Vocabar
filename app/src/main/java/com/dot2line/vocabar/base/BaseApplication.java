47ade5678b5ced9a9411eedbadf84c53248eaaf0
dot2line <yands11@gmail.com>
2017-11-28 01:09:45 +0900

    feat: fabric setting

--- app/src/main/java/com/dot2line/vocabar/base/BaseApplication.java
+++ app/src/main/java/com/dot2line/vocabar/base/BaseApplication.java
@@ -2,6 +2,8 @@ package com.dot2line.vocabar.base;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 import io.realm.Realm;
 
 public class BaseApplication extends Application {
@@ -9,6 +11,7 @@ public class BaseApplication extends Application {
   @Override
   public void onCreate() {
     super.onCreate();
+    Fabric.with(this, new Crashlytics());
 
     Realm.init(this);
   }
